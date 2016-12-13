9a715f2b51eed3674342643940b41b5c8f01108f
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-12-13 21:32:38 +0900

    Split

--- bruteforce.exs
+++ bruteforce.exs
@@ -1,16 +1,18 @@
 defmodule BruteForce do
   def run do
-    lower_chars
+    chars = lower_chars
+    read(chars, String.length(chars))
+#    upper_chars
   end
 
   defp lower_chars do
     a = 97
     z = 26
-    to_string [span(a, z)]
+    to_string([span(a, z)])
   end
   
   defp upper_chars do
-    a = 1
+    a = 65
     z = 26
     to_string [span(a, z)]
   end
@@ -19,9 +21,16 @@ defmodule BruteForce do
     []
   end
   defp span(head, target_loc) do
-    IO.puts head
-    IO.puts target_loc
     [ head | span(head + 1, target_loc - 1)]
   end
 
+  defp read(str, 0) do
+    IO.puts str
+  end
+
+  defp read(str, length) do
+    IO.puts String.first str
+    chars = String.slice(str, 1, String.length(str) - 1)
+    read(chars, String.length(chars))
+  end
 end
