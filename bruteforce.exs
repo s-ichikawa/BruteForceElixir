c6db05ee05c412415b789e1a1e7559bbc29d8b52
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-12-17 10:49:21 +0900

    multi chars

--- bruteforce.exs
+++ bruteforce.exs
@@ -1,19 +1,21 @@
 defmodule BruteForce do
   def run do
-    Enum.map(String.graphemes(lower_chars), &IO.puts(&1))
-    Enum.map(String.graphemes(upper_chars), &IO.puts(&1))
+    Enum.map(lower_chars, &IO.puts(&1))
+    Enum.map(upper_chars, &IO.puts(&1))
+    Enum.map(lower_chars, &multi(lower_chars, &1))
+    Enum.map(lower_chars, &multi(upper_chars, &1))
   end
 
   defp lower_chars do
     a = 97
     z = 26
-    to_string([span(a, z)])
+    String.graphemes(to_string([span(a, z)]))
   end
   
   defp upper_chars do
     a = 65
     z = 26
-    to_string [span(a, z)]
+    String.graphemes(to_string [span(a, z)])
   end
 
   defp span(head, 0) do
@@ -23,5 +25,10 @@ defmodule BruteForce do
     [ head | span(head + 1, target_loc - 1)]
   end
 
+  defp multi(chars, char) do
+    Enum.map(chars, fn(saffix) ->
+        IO.puts(char <> saffix)
+    end)
+  end
 
 end
